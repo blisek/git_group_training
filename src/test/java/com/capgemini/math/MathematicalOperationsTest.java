46cd12813f78c103da7e8d63a01f9fda351755fc
michalchrapczynski <michal.chrapczynski@gmail.com>
2015-10-23 13:07:59 +0200

    Add check parity number and dependancy fest-assert

--- src/test/java/com/capgemini/math/MathematicalOperationsTest.java
+++ src/test/java/com/capgemini/math/MathematicalOperationsTest.java
@@ -2,6 +2,7 @@ package com.capgemini.math;
 
 import static org.junit.Assert.*;
 
+import org.fest.assertions.Assertions;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -20,10 +21,32 @@ public class MathematicalOperationsTest {
 	public void testAbs() {
 		assertNotEquals(0, MathematicalOperations.abs(Integer.MIN_VALUE));
 		assertNotEquals(Integer.MIN_VALUE, MathematicalOperations.abs(Integer.MIN_VALUE));
-		for(int i = 0; i < 12; ++i) {
-			int pow2 = (int)Math.pow(2, i);
+		for (int i = 0; i < 12; ++i) {
+			int pow2 = (int) Math.pow(2, i);
 			assertEquals(pow2, MathematicalOperations.abs(-pow2));
 		}
+	}
+
+	@Test
+	public void testNumberIsParity() {
+		// given
+		int number = 6;
+
+		// when
+		boolean result = MathematicalOperations.parityNumber(number);
+		// then
+		Assertions.assertThat(result).isTrue();
+	}
+
+	@Test
+	public void testNumberIsNotParity() {
+		// given
+		int number = 5;
+
+		// when
+		boolean result = MathematicalOperations.parityNumber(number);
+		// then
+		Assertions.assertThat(result).isFalse();
 	}
 
 }

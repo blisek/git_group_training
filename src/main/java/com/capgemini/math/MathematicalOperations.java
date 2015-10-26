a9f468b1686dd5d8a1b08c424da40e2fefb33f4a
michalchrapczynski <michal.chrapczynski@gmail.com>
2015-10-26 10:14:04 +0100

    Revert "Add check parity number and dependancy fest-assert"
    This reverts commit 46cd12813f78c103da7e8d63a01f9fda351755fc.

--- src/main/java/com/capgemini/math/MathematicalOperations.java
+++ src/main/java/com/capgemini/math/MathematicalOperations.java
@@ -6,8 +6,4 @@ public class MathematicalOperations {
 		long castValue = value;
 		return value < 0 ? -castValue : castValue;
 	}
-
-	public static boolean parityNumber(int value) {
-		return value % 2 == 0 ? true : false;
-	}
 }

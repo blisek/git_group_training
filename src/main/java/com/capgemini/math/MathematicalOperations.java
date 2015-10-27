8db1caf671f6370dac9469a39e67f7ed723fa4cb
dominikapurszkee <purszke.d@.wp.pl>
2015-10-27 09:41:00 +0100

    division with rounding

--- src/main/java/com/capgemini/math/MathematicalOperations.java
+++ src/main/java/com/capgemini/math/MathematicalOperations.java
@@ -1,9 +1,19 @@
 package com.capgemini.math;
 
+import java.math.RoundingMode;
+
+import com.google.common.math.IntMath;
+
 public class MathematicalOperations {
 
 	public static long abs(int value) {
 		long castValue = value;
 		return value < 0 ? -castValue : castValue;
+	}
+
+	public static int divisionWithRounding(int value1, int value2, RoundingMode mode) {
+
+		return IntMath.divide(value1, value2, mode);
+
 	}
 }
--- src/test/java/com/capgemini/math/MathematicalOperationsTest.java
+++ src/test/java/com/capgemini/math/MathematicalOperationsTest.java
@@ -1,6 +1,9 @@
 package com.capgemini.math;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.math.RoundingMode;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -20,10 +23,34 @@ public class MathematicalOperationsTest {
 	public void testAbs() {
 		assertNotEquals(0, MathematicalOperations.abs(Integer.MIN_VALUE));
 		assertNotEquals(Integer.MIN_VALUE, MathematicalOperations.abs(Integer.MIN_VALUE));
-		for(int i = 0; i < 12; ++i) {
-			int pow2 = (int)Math.pow(2, i);
+		for (int i = 0; i < 12; ++i) {
+			int pow2 = (int) Math.pow(2, i);
 			assertEquals(pow2, MathematicalOperations.abs(-pow2));
 		}
+	}
+
+	@Test
+	public void testDivisionModeCeiling() {
+
+		// given
+		int x = 7;
+		int y = 4;
+		int result = 2;
+		// when,then
+		assertEquals(result, MathematicalOperations.divisionWithRounding(x, y, RoundingMode.CEILING));
+
+	}
+
+	@Test
+	public void testDivisionModeDown() {
+
+		// given
+		int x = 7;
+		int y = 4;
+		int result = 1;
+		// when,then
+		assertEquals(result, MathematicalOperations.divisionWithRounding(x, y, RoundingMode.DOWN));
+
 	}
 
 }
